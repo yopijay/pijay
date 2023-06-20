f898fe4602be7c2615a50355a628b73284f0664d
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-06-20 16:57:48 +0800

    Re-design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ const Index = () => {
     return (
         <Box display= "flex">
             <Container maxWidth= "xl">
-                
+                <Content />
             </Container>
         </Box>
     );
