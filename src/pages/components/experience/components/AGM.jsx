1e195f3ce817d610019c29cb8ae3b6c16946021f
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-06-26 16:39:23 +0800

    Experience

--- src/pages/components/experience/components/AGM.jsx
+++ src/pages/components/experience/components/AGM.jsx
@@ -1,21 +1,21 @@
 // Libraries
-import { Box, Grid, Stack, Typography } from "@mui/material";
+import { Box, Chip, Grid, Stack, Typography } from "@mui/material";
 
 // Constants
-import { year } from "../index.style"; // Styles
+import { company, info, label, position, year } from "../index.style"; // Styles
 
 const AGM = () => {
     return (
         <Box>
-            <Grid container direction= "row" justifyContent= "flex-start" alignItems= "flex-start" spacing= { 2 }>
+            <Grid container direction= "row" justifyContent= "flex-start" alignItems= "flex-start">
                 <Grid item xs= { 12 } sm= { 4 }><Typography sx= { year }>June 2019 - Sept 2021</Typography></Grid>
                 <Grid item xs= { 12 } sm= { 8 }>
                     <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 2 }>
-                        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 1 }>
-                            <Typography>Fullstack Web Developer</Typography>
-                            <Typography>KC Electrical Innovation, Inc.</Typography>
+                        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch">
+                            <Typography sx= { position }>Frontend Web Developer</Typography>
+                            <Typography sx= { company }>Aktus Global Management, Inc.</Typography>
                         </Stack>
-                        <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eros diam, dignissim ac sem non, volutpat pulvinar elit. 
+                        <Typography sx= { info }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eros diam, dignissim ac sem non, volutpat pulvinar elit. 
                             Nunc venenatis facilisis ipsum, eu pharetra magna porta aliquam. Nulla vel ante est. Nullam vehicula imperdiet cursus. 
                             In feugiat, nisl et vulputate elementum, felis turpis faucibus tellus, et feugiat turpis enim eget purus. Nulla accumsan efficitur ante eu porttitor. 
                             In rutrum convallis posuere. Donec feugiat faucibus enim at finibus. Maecenas tortor mi, imperdiet non eleifend aliquet, tincidunt quis eros. 
@@ -23,10 +23,29 @@ const AGM = () => {
                             Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in consectetur velit.</Typography>
                         <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 2 }>
                             <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch">
-                                <Typography gutterBottom>Tech Stack</Typography>
+                                <Typography gutterBottom sx= { label }>Tech Stack</Typography>
+                                <Grid container direction= "row" justifyContent= "flex-start" alignItems= "flex-start" spacing= { 1 }>
+                                    <Grid item><Chip label= "ReactJS" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Laravel" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Java" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "MaterialUI" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Bootstrap" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "NodeJS" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "HTML" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "CSS" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "OOP" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "PHP" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Sass/Scss" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                </Grid>
                             </Stack>
                             <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch">
-                                <Typography gutterBottom>Tools</Typography>
+                                <Typography gutterBottom sx= { label }>Tools</Typography>
+                                <Grid container direction= "row" justifyContent= "flex-start" alignItems= "flex-start" spacing= { 1 }>
+                                    <Grid item><Chip label= "VSCode" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Android Studio" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Github" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                    <Grid item><Chip label= "Jira" sx= {{ backgroundColor: '#263238', color: '#ECEFF1' }} /></Grid>
+                                </Grid>
                             </Stack>
                         </Stack>
                     </Stack>
--- src/pages/components/experience/index.style.js
+++ src/pages/components/experience/index.style.js
@@ -9,15 +9,48 @@ export const title = {
 }
 
 export const year = {
-    color: '#455A64',
-    fontWeight: 'bolder'
+    color: '#B0BEC5',
+    fontWeight: 'bolder',
+    fontSize: {
+        xs: '110%',
+        md: '100%'
+    },
+    transition: 'all 0.2s ease-in-out'
 }
 
-// export const info = {
-//     color: '#e4f7f8',
-//     transition: 'all 0.2s ease-in-out',
-//     fontSize: {
-//         xs: '110%',
-//         md: '100%'
-//     }
-// }
+export const info = {
+    color: '#78909C',
+    transition: 'all 0.2s ease-in-out',
+    fontSize: {
+        xs: '110%',
+        md: '100%'
+    }
+}
+
+export const position = {
+    color: '#a8cad3',
+    fontSize: {
+        xs: '120%',
+        sm: '125%',
+        md: '130%'
+    },
+    transition: 'all 0.2s ease-in-out'
+}
+
+export const company = {
+    fontSize: {
+        xs: '105%',
+        sm: '110%',
+        md: '115%'
+    },
+    color: '#455A64',
+    fontWeight: 'bold',
+    transition: 'all 0.2s ease-in-out'
+}
+
+export const label = {
+    color: '#B0BEC5',
+    fontWeight: 'bolder',
+    fontSize: '100%',
+    transition: 'all 0.2s ease-in-out'
+}
