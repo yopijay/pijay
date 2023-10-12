18493b2e96f1afc2eda199c777a91d43c5fe2140
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-10-12 13:25:41 +0800

    Reset

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,10 @@
 // Libraries
-import { Box, Container, Stack } from "@mui/material";
-
-// Components
-import Intro from "./components/intro";
-import About from "./components/about";
-import Experience from "./components/experience";
-import Works from "./components/works";
-
-// Constants
-import { container } from "./index.style"; // Styles
+import { Container } from "@mui/material";
 
 const Index = () => {
     return (
-        <Box display= "flex">
-            <Container maxWidth= "md">
-                <Stack sx= { container }>
-                    <Intro />
-                    <About />
-                    <Experience />
-                    <Works />
-                </Stack>
-            </Container>
-        </Box>
+        <Container maxWidth= "md">
+        </Container>
     );
 }
 
