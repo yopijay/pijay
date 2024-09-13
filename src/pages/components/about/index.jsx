50aae601b656d003d327093adf4ded549ac3696c
Pijay <53463564+yopijay@users.noreply.github.com>
2024-09-13 12:37:48 +0800

    My Works section
    - Initial design

--- src/pages/components/about/index.jsx
+++ src/pages/components/about/index.jsx
@@ -11,7 +11,7 @@ const Index = () => {
             <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 2 }>
                 <Typography sx= { info }>I have been in the industry for 5 years, creating softwares, specifically web apps. I am currently working on a system
                     which is RSVP, for my personal need, and later on if it becomes successfull, I also plan to use it for random users who also have the same needs
-                    as me.</Typography>
+                    as mine.</Typography>
             </Stack>
         </Stack>
     );
--- src/pages/components/socials/index.jsx
+++ src/pages/components/socials/index.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 // Constants
 const socials = [
     { links: 'https://www.facebook.com/yopijay', icon: <FontAwesomeIcon icon= { brands('facebook') } /> },
-    { links: 'https://instagram.com/kuyapijay?igshid=MzNINGNkZWQ4Mg==', icon: <FontAwesomeIcon icon= { brands('instagram') } /> },
+    { links: 'https://www.instagram.com/pijaayyyy__/?next=%2F', icon: <FontAwesomeIcon icon= { brands('instagram') } /> },
     { links: 'https://x.com/Pijjaaayyyy?t=xB4x3nmXSXayhMs2XLoVBQ&s=09', icon: <FontAwesomeIcon icon= { brands('twitter') } /> },
     { links: 'https://www.youtube.com/@pijay8957', icon: <FontAwesomeIcon icon= { brands('youtube') } /> },
     { links: 'https://www.linkedin.com/in/paul-john-judan-0060a41b9/', icon: <FontAwesomeIcon icon= { brands('linkedin') } /> },
@@ -17,7 +17,7 @@ const socials = [
 const links = {
     fontSize: '145%', 
     textDecoration: 'none', 
-    color: '#D8D9DA',
+    color: '#FFF6E0',
     '&:hover': { 
         color: '#FFFFFF', 
         transition: 'all 0.2s ease-in-out' 
